740220358099fb7b1663087fc2d40e271002437d
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2024-04-02 23:08:12 +0200

    hide keyboard on startviewcontroller

--- SwiftKeysApp/ViewControllers/StartViewController.swift
+++ SwiftKeysApp/ViewControllers/StartViewController.swift
@@ -17,35 +17,51 @@ class StartViewController: UIViewController {
     var highscoreSegueKey = "highscoreSegue"
     var gameSegueKey = "gameSegue"
     var gameManager = GameManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.   
         
         
+        //Add a done button to playerName textfield
+        addDoneButtonOnKeyboard(to: playerNameField)
         
         
+    }
+    func addDoneButtonOnKeyboard(to textField: UITextField) {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneClicked))
+
+        toolbar.setItems([flexibleSpace, doneButton], animated: false)
+        textField.inputAccessoryView = toolbar
+    }
+
+    @objc func doneClicked() {
+        //make keyboard invisible
+        view.endEditing(true)
     }
     
     func showAlert() {
         // Skapa en alert controller med titel och meddelande
         let alertController = UIAlertController(title: "Ange spelarnamn", message: "Du måste ange ett namn för att kunna spela", preferredStyle: .alert)
-
+        
         // Skapa en åtgärd för alert kontrollern
         let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
-     
+            
         }
-
+        
         // Lägg till åtgärden till alert kontrollern
         alertController.addAction(okAction)
-
+        
         // Presentera alert kontrollern
         self.present(alertController, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == gameSegueKey{
-
+            
             let difficulty = levelSelector.selectedSegmentIndex
             gameManager.setDifficulty(level: difficulty)
             
@@ -54,7 +70,7 @@ class StartViewController: UIViewController {
             
         }
     }
-
+    
     @IBAction func playButtonPressed(_ sender: UIButton) {
         
         let name = playerNameField.text
